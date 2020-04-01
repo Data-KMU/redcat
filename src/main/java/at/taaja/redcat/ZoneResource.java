28e53d179909e356e990c175d8f57c0463b73fe7
kris <kris.raich@gmail.com>
2020-04-01 15:02:36 +0200

    disabled debug

--- src/main/java/at/taaja/redcat/ZoneResource.java
+++ src/main/java/at/taaja/redcat/ZoneResource.java
@@ -29,42 +29,39 @@ public class ZoneResource {
     }
 
 
-
-    @POST
-    @Path("extension")
-    public Response addExtension(AbstractExtension abstractExtension) {
-        this.zoneRepository.insertExtension(abstractExtension);
-        return Response.ok().build();
-    }
-
+//    @POST
+//    @Path("extension")
+//    public Response addExtension(AbstractExtension abstractExtension) {
+//        this.zoneRepository.insertExtension(abstractExtension);
+//        return Response.ok().build();
+//    }
 
 
-
-    @SneakyThrows
-    @POST
-    @Path("test")
-    public Response test(String s){
-        ObjectMapper objectMapper = new ObjectMapper();
-        ObjectReader objectReader = objectMapper.reader();
-
-        String topic = "spatial-life-data-c56b3543-6853-4d86-a7bc-1cde673a5582";
-
-        String id = this.getIdFromTopic(topic);
-
-        Object extension = this.zoneRepository.getExtensionAsObject(id);
-
-        ObjectReader updater = objectMapper.readerForUpdating(extension);
-        Object updatedExtension = updater.readValue(s);
-
-        AbstractExtension abstractExtension = objectMapper.convertValue(updatedExtension, AbstractExtension.class);
-
-        this.zoneRepository.update(id, updatedExtension);
-
-        return Response.ok().build();
-    }
-
-    private String getIdFromTopic(String topic){
-        return topic.substring(Topics.SPATIAL_EXTENSION_LIFE_DATA_TOPIC_PREFIX.length());
-    }
+//    @SneakyThrows
+//    @POST
+//    @Path("test")
+//    public Response test(String s){
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        ObjectReader objectReader = objectMapper.reader();
+//
+//        String topic = "spatial-life-data-c56b3543-6853-4d86-a7bc-1cde673a5582";
+//
+//        String id = this.getIdFromTopic(topic);
+//
+//        Object extension = this.zoneRepository.getExtensionAsObject(id);
+//
+//        ObjectReader updater = objectMapper.readerForUpdating(extension);
+//        Object updatedExtension = updater.readValue(s);
+//
+//        AbstractExtension abstractExtension = objectMapper.convertValue(updatedExtension, AbstractExtension.class);
+//
+//        this.zoneRepository.update(id, updatedExtension);
+//
+//        return Response.ok().build();
+//    }
+//
+//    private String getIdFromTopic(String topic){
+//        return topic.substring(Topics.SPATIAL_EXTENSION_LIFE_DATA_TOPIC_PREFIX.length());
+//    }
 
 }
